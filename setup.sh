b8cc3a3a9b6c334032670810ddc7b0eba40eafad
Ritaank Tiwari <ritaank@gmail.com>
2023-03-20 20:12:10 -0400

    setup lfs 1

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm -rf temp
-mkdir temp
-curl https://download.is.tue.mpg.de/soma/tutorials/SOMA_FOLDER_TEMPLATE.tar.bz2 --output ./temp/SOMA_FOLDER_TEMPLATE.tar.bz2
-tar -xjf ./temp/SOMA_FOLDER_TEMPLATE.tar.bz2 -C ./
-mv SOMA_FOLDER_TEMPLATE $1
+# rm -rf temp
+# mkdir temp
+# curl https://download.is.tue.mpg.de/soma/tutorials/SOMA_FOLDER_TEMPLATE.tar.bz2 --output ./temp/SOMA_FOLDER_TEMPLATE.tar.bz2
+# tar -xjf ./temp/SOMA_FOLDER_TEMPLATE.tar.bz2 -C ./
+# mv SOMA_FOLDER_TEMPLATE $1
 
-
+curl https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=smplx_locked_head.tar.bz2 --output ./temp/smplx_locked_head.tar.bz2
+tar -xjf ./temp/smplx_locked_head.tar.bz2 -C ./$1/support_files/smplx
