e8114a5a8d7135b2693ec62c13ced405f8b80094
Ritaank Tiwari <ritaank@gmail.com>
2023-06-13 13:17:53 -0400

    various updates

--- setup.py
+++ setup.py
@@ -24,26 +24,6 @@ def copy_dirs(args):
             return
     
         logger.success(f"Successfully copied {dir} to .")
-        
-    # path_to_soma_base = osp.join(args.studies_path, '_Repositories','soma-setup', 'soma-root')
-    # logger.info("copying soma-root from {} to {}".format(path_to_soma_base, '.'))
-    # try:
-    #     subprocess.run(['cp', '-R', path_to_soma_base, '.'])
-    # except:
-    #     logger.error('Could not find soma-root. Please check the path to the studies folder')
-    #     return
-
-    # logger.success("Successfully copied soma-root to {}".format(args.folder_name))
-
-    # path_to_smpl_fast = osp.join(args.studies_path, '_Repositories','soma-setup', 'smpl-fast-derivatives.tar.bz2')
-    # logger.info("copying soma-root from {} to {}".format(path_to_smpl_fast, '.'))
-    # try:
-    #     subprocess.run(['cp', '-R', path_to_smpl_fast, '.'])
-    # except:
-    #     logger.error('Could not find smpl-fast-derivatives. Please check the path to the studies folder')
-    #     return
-
-    # logger.success("Successfully copied smpl-fast-derivatives to {}".format(args.folder_name))
 
 def setup(args):
     copy_dirs(args)
